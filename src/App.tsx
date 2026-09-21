7b73200193c8804f09f39c22a2b62aae00d235ad
pulx88 <kiku88@gmail.com>
2026-09-21 18:53:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -666,7 +666,7 @@ export default function App() {
         onClose={() => setIsExportImportModalOpen(false)}
         onBooksUpdated={async (newBooks) => {
   setBooks(newBooks);
-  await dbService.saveBooks(newBooks);
+  await dbService.saveAllBooks(newBooks);
 }}
       />
     </div>
